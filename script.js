1eaee217392e6ea25b7b384043391c7f0e86b8bb
Jonathon Wilks <jonathondwilks@icloud.com>
2021-06-02 16:25:10 +0100

    Search works (yey!) - added a reset button - done a bit of styling (which is what i like best!)

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ state = {
   favourits: [],
 };
 
-// ⬇ query selector for html bridge⬇ //////////////////////////////////////////////////
+// ⬇ query selectors for html bridge⬇ //////////////////////////////////////////////////
 
 const mainElement = document.querySelector("#main");
-console.log("is this working :", mainElement);
+const header = document.querySelector("#header");
+const footer = document.querySelector("#footer");
 
 // ⬇ function to create 1 user chip ⬇ /////////////////////////////////////////////////
 
@@ -23,10 +24,7 @@ function renderCharacterChip(character) {
 
   const characterImgEl = document.createElement("img");
   characterImgEl.setAttribute("class", "charImage");
-  characterImgEl.setAttribute(
-    "src",
-    "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-  );
+  characterImgEl.setAttribute("src", character.image);
   characterImgEl.setAttribute("alt", "Rick");
 
   const mainUl = document.createElement("ul");
@@ -34,25 +32,27 @@ function renderCharacterChip(character) {
 
   const nameLi = document.createElement("li");
   nameLi.setAttribute("class", "nameLi");
-  nameLi.innerText = "Rick Sanchez";
+  nameLi.innerText = `Name: ${character.name}`;
 
   const statusLi = document.createElement("li");
   statusLi.setAttribute("class", "statusLi");
-  statusLi.innerText = "Alive";
+  statusLi.innerText = `Status: ${character.status}`;
 
   const speciesLi = document.createElement("li");
   speciesLi.setAttribute("class", "speciesLi");
-  speciesLi.innerText = "Human";
+  speciesLi.innerText = `Species: ${character.species}`;
 
   const genderLi = document.createElement("li");
   genderLi.setAttribute("class", "genderLi");
-  genderLi.innerText = "Male";
+  genderLi.innerText = `Gender: ${character.gender}`;
 
   const favouriteLabel = document.createElement("label");
+  favouriteLabel.setAttribute("class", "favourite");
   favouriteLabel.setAttribute("for", "Favourite");
-  favouriteLabel.innerText = "Favour meeee ";
+  favouriteLabel.innerText = "Favour Meeee ";
 
   const favouriteCheckbox = document.createElement("input");
+  favouriteCheckbox.setAttribute("class", "favouriteCheckBox");
   favouriteCheckbox.setAttribute("type", "checkbox");
   favouriteCheckbox.setAttribute("name", "favouriteCheckBox");
   favouriteCheckbox.setAttribute("value", "input");
@@ -60,21 +60,84 @@ function renderCharacterChip(character) {
   mainUl.append(nameLi, statusLi, speciesLi, genderLi);
   characterImage.append(characterImgEl, mainUl);
 
-  characterChipEl.append(
-    characterImage,
-
-    favouriteLabel,
-    favouriteCheckbox
-  );
+  characterChipEl.append(characterImage, favouriteLabel, favouriteCheckbox);
   mainElement.append(characterChipEl);
 }
 
-// ⬇ function to create all the user chips ⬇ //( not working)//////////////////////////
+// ⬇ function to create all the user chips ⬇ /////////////////////////////////////////
 
 function renderCharacterChips(characters) {
   for (const character of state.character.results) {
     renderCharacterChip(character);
   }
+}
+
+// ⬇ function to add the top function ability ⬇ /////////////////////////////////////////
+// trying to add in document.getElementsByClassName("topSearchBar").reset(); to reset form - not working
+
+function addSearchFacilty() {
+  const searchEl = document.createElement("input");
+  searchEl.setAttribute("class", "topSearchBar");
+
+  searchEl.setAttribute("type", "text");
+  searchEl.setAttribute("placeholder", "Search Characters...");
+
+  const buttonDiv = document.createElement("div");
+  buttonDiv.setAttribute("class", "buttonContainer");
+
+  const buttonSearchEl = document.createElement("button");
+  buttonSearchEl.setAttribute("class", "buttonSearchBar");
+  buttonSearchEl.innerText = "Hit Me To Search";
+
+  buttonSearchEl.addEventListener("click", function () {
+    console.log("you've clicked the button - well done - good boy");
+
+    fetch(`https://rickandmortyapi.com/api/character/?name=${searchEl.value}`)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (characters) {
+        mainElement.innerHTML = "";
+        state.character = characters;
+        renderCharacterChips(console.log("inside function", characters));
+      });
+  });
+
+  const buttonResetEl = document.createElement("button");
+  buttonResetEl.setAttribute("class", "buttonResetBar");
+  buttonResetEl.innerText = "Hit Me To Reset";
+
+  buttonResetEl.addEventListener("click", function () {
+    console.log("you've clicked the second button - well done - good boy");
+
+    mainElement.innerHTML = "";
+    getCharactersFromServer();
+  });
+
+  buttonDiv.append(buttonSearchEl, buttonResetEl);
+  header.append(searchEl, buttonDiv);
+}
+
+// ⬇ function to add the bottom button 'more' ability ⬇ /////////////////////////////////////////
+
+function addMoreButton() {
+  const moreButton = document.createElement("button");
+  moreButton.setAttribute("class", "moreButton");
+  moreButton.innerText = "Click for more Characters";
+  footer.append(moreButton);
+
+  moreButton.addEventListener("click", function (character) {
+    fetch("https://rickandmortyapi.com/api/character?page=2")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (characters) {
+        state.character = characters;
+        renderCharacterChips(characters);
+      });
+
+    console.log("you clicked more button - good boy - well done - very clever");
+  });
 }
 
 // ⬇ functions to get info from server and store in state ⬇ //////////////////////////
@@ -114,7 +177,8 @@ function runFunctions() {
   getCharactersFromServer();
   getLocationFromServer();
   getEpisodeFromServer();
-  // renderCharacterChip();
+  addSearchFacilty();
+  addMoreButton();
 }
 
 runFunctions();
